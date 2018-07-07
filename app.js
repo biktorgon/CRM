5f3ff2c4bdb73d0646621565a3e4c87bda9f73b9
biktorgon <biktorgon@mail.ru>
2018-07-07 13:14:44 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cors = require('cors');
+const logger = require('morgan');
+const keys = require('./config/keys');
+const app = express();
+
+mongoose.connect(keys.mongoURI,{ useNewUrlParser: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(error => console.log(error));
+
+
+app.use(passport.initialize());
+require('./middleware/passport')(passport);
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json({limit: '1mb'}));
+app.use(bodyParser.urlencoded({extend: true}));
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/analytics', require('./routes/analytics'));
+app.use('/api/category', require('./routes/category'));
+app.use('/api/order', require('./routes/order'));
+app.use('/api/position', require('./routes/position'));
+
+app.use((req, res) => res.status(404).send('Not found'));
+
+module.exports = app;
---
+++ controllers/auth.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const keys = require('../config/keys');
+
+
+module.exports.register = async function (req, res) {
+    //email password
+    const confirmUser = await User.findOne({email: req.body.email});
+
+    if (confirmUser) {
+        res.status(409).json({
+            message: 'email already exist'
+        });
+    } else {
+        const salt = bcrypt.genSaltSync(10);
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, salt)
+        });
+
+        try {
+            await user.save();
+            res.status(201).json(user);
+        } catch (e) {
+
+        }
+    }
+};
+
+module.exports.login = async function (req, res) {
+   const confirmUser = await User.findOne({email: req.body.email});
+
+   if (confirmUser) {
+       const passwordResult = bcrypt.compareSync(req.body.password, confirmUser.password);
+       if (passwordResult) {
+           const token = jwt.sign({
+               email: confirmUser.email,
+               userId: confirmUser._id
+           }, keys.jwt, {expiresIn: 60 * 60});
+           res.status(200).json({
+               token: token
+           });
+       } else {
+           res.status(401).json({
+               message: 'Password is incorrect'
+           });
+       }
+   } else {
+       res.status(404).json({
+           message: 'user not found'
+       });
+   }
+};
