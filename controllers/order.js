a78412f487c14db5e591b000bbd0cd0b7ecbed78
biktorgon <biktorgon@mail.ru>
2018-07-11 17:23:56 +0300

    add orders

--- controllers/order.js
+++ controllers/order.js
@@ -1,7 +1,59 @@
-module.exports.getAll = function (req, res) {
+const order = require('../models/Order');
+const errorHandler = require('../utils/errorHanler');
 
+module.exports.getAll = async function (req, res) {
+    try {
+
+        const query = {
+            user: req.user.id
+        };
+
+        if (req.query.start) {
+            query.date = {
+                //greater then equal
+                $gte: req.query.start
+            }
+        }
+
+        if (req.query.end) {
+            query.date = !query.date ? {} : query.date;
+            //less then equal
+            query.date['$lte'] = req.query.end;
+        }
+
+        if (req.query.order) {
+            query.order = +req.query.order;
+        }
+
+        const orders = await Order
+            .find(query)
+            .sort({data: -1})
+            .skip(+req.query.offset)
+            .limit(+req.query.limit);
+
+        res.status(200).json(orders);
+
+    } catch (e) {
+        errorHandler(e);
+    }
 };
 
-module.exports.create = function (req, res) {
+module.exports.create = async function (req, res) {
+    try {
+        const lastOrder = await Order
+            .findOne({user: req.user.id})
+            .sort({date: -1}); //date DESC
 
+        const maxOrder = lastOrder ? lastOrder.order : 0;
+
+        const order = await new Order({
+            list: req.body.list,
+            user: req.user.id,
+            order: maxOrder + 1
+        }).save();
+
+        res.status(201).json(order);
+    } catch (e) {
+        errorHandler(e);
+    }
 };
